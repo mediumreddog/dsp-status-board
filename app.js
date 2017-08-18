7f7540eafc7b192a2334b991192a312b083bcb37
Martin <cdmfk6@r08itrss.managed.mst.edu>
2017-08-18 15:59:03 -0500

    Began app implementation. Finished up chair announcements.

---
+++ app.js
@@ -0,0 +1,23 @@
+class app {
+  constructor(name) {
+    this.name = name;
+
+    this.renderIcon();
+  }
+
+  renderIcon() {
+    var html = '<div class="appContainer">\
+    							<span data-toggle="modal" data-target="#appModal" href="#"><img class="app" src="apps/' + this.name + '/icon.png" /></span>\
+    							<div>' + this.name.charAt(0).toUpperCase() + this.name.slice(1) + '</div>\
+    						</div>';
+
+    $("#appDock").append(html);
+  }
+
+}
+
+$(document).ready(function () {
+  new app('weather');
+  new app('flickr');
+
+});
--- statusboard.js
+++ statusboard.js
@@ -1,131 +1,139 @@
 function updateClock() {
-    var currentTime = new Date ( );
+	var currentTime = new Date();
 
-    var time = timeString(currentTime);
+	var time = timeString(currentTime);
 
-    $("#clockNum").html(time[0]);
-    $("#clockAMPM").html(time[1]);
+	$("#clockNum").html(time[0]);
+	$("#clockAMPM").html(time[1]);
 
-    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    var currentDateString = days[currentTime.getDay()] + ", " + months[currentTime.getMonth()] + " " + currentTime.getDate();
+	var currentDateString = days[currentTime.getDay()] + ", " + months[currentTime.getMonth()] + " " + currentTime.getDate();
 
-    $("#clockDate").html(currentDateString);
+	$("#clockDate").html(currentDateString);
 }
 
 function loadDSPCalendar() {
-  $.getJSON("https://www.googleapis.com/calendar/v3/calendars/dspeboard%40gmail.com/events?singleEvents=true&timeMin="+new Date(Date.now()).toISOString()+"&orderBy=startTime&key=AIzaSyDyq-41653zwrojWvFKnuzM6L9Kh0DJk7s&maxResults=10", function(data) {
-    $.each(data["items"], function(key, value) {
+	$.getJSON("https://www.googleapis.com/calendar/v3/calendars/dspeboard%40gmail.com/events?singleEvents=true&timeMin=" + new Date(Date.now()).toISOString() + "&orderBy=startTime&key=AIzaSyDyq-41653zwrojWvFKnuzM6L9Kh0DJk7s&maxResults=10", function (data) {
+		$.each(data["items"], function (key, value) {
 
-      displayEvent("DSPCAL", value);
-    });
+			displayEvent("DSPCAL", value);
+		});
 
-  });
+	});
 }
 
 function loadATHCalendar() {
-  $.getJSON("https://www.googleapis.com/calendar/v3/calendars/dsp.athletics%40gmail.com/events?singleEvents=true&timeMin="+new Date(Date.now()).toISOString()+"&orderBy=startTime&key=AIzaSyDyq-41653zwrojWvFKnuzM6L9Kh0DJk7s&maxResults=10", function(data) {
+	$.getJSON("https://www.googleapis.com/calendar/v3/calendars/dsp.athletics%40gmail.com/events?singleEvents=true&timeMin=" + new Date(Date.now()).toISOString() + "&orderBy=startTime&key=AIzaSyDyq-41653zwrojWvFKnuzM6L9Kh0DJk7s&maxResults=10", function (data) {
 
-    if(data["items"].length == 0) {
-      $("#ATHCAL").append('<p class="noItems">No Upcoming Events</p>');
-    } else {
-      $.each(data["items"], function(key, value) {
-        displayEvent("ATHCAL", value);
-      });
-    }
-  });
+		if (data["items"].length == 0) {
+			$("#ATHCAL").append('<p class="noItems">No Upcoming Events</p>');
+		} else {
+			$.each(data["items"], function (key, value) {
+				displayEvent("ATHCAL", value);
+			});
+		}
+	});
 }
 
 function updateCalendars() {
-  $("#ATHCAL").empty();
-  loadATHCalendar()
+	$("#ATHCAL").empty();
+	loadATHCalendar()
 
-  delete  displayEvent.month;
+	delete displayEvent.month;
 
-  $("#DSPCAL").empty();
-  loadDSPCalendar()
+	$("#DSPCAL").empty();
+	loadDSPCalendar()
 }
 
 function timeString(currentTime) {
-  var ret = {};
+	var ret = {};
 
-  var currentHours = currentTime.getHours ( );
-  var currentMinutes = currentTime.getMinutes ( );
+	var currentHours = currentTime.getHours();
+	var currentMinutes = currentTime.getMinutes();
 
-  ret[1] = ( currentHours < 12 ) ? "AM" : "PM";
+	ret[1] = (currentHours < 12) ? "AM" : "PM";
 
-  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+	currentMinutes = ((currentMinutes < 10) ? "0" : "") + currentMinutes;
 
-  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+	currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+	currentHours = (currentHours == 0) ? 12 : currentHours;
 
-  ret[0] = currentHours + ":" + currentMinutes;
+	ret[0] = currentHours + ":" + currentMinutes;
 
-  return ret;
+	return ret;
 }
 
 function displayEvent(calendar, values) {
 
-  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-  if("dateTime" in values["start"]) {
-    var date = new Date(values["start"]["dateTime"]);
-    var day = date.getDate();
-    var time = timeString(date);
-    time = time[0] + " " + time[1];
+	if ("dateTime" in values["start"]) {
+		var date = new Date(values["start"]["dateTime"]);
+		var day = date.getDate();
+		var time = timeString(date);
+		time = time[0] + " " + time[1];
 
-  } else {
-    var date = new Date(values["start"]["date"]);
-    var day = date.getDate();
-    var time = "ALL DAY";
+	} else {
+		var date = new Date(values["start"]["date"]);
+		var day = date.getDate();
+		var time = "ALL DAY";
 
-  }
+	}
 
-  if(!displayEvent.month || displayEvent.month != date.getMonth()) {
-    displayEvent.month = date.getMonth();
-    $("#"+calendar).append('<h5 class="calendarMonth">' + months[displayEvent.month] + '</h5>');
-  }
+	if (!displayEvent.month || displayEvent.month != date.getMonth()) {
+		displayEvent.month = date.getMonth();
+		$("#" + calendar).append('<h5 class="calendarMonth">' + months[displayEvent.month] + '</h5>');
+	}
 
 
-  $("#"+calendar).append('\
+	$("#" + calendar).append('\
   <li>\
     <span class="calendarDate">' + day + '</span>\
-    <span class="calendarEvent">' + values["summary"]  + '</span>\
+    <span class="calendarEvent">' + values["summary"] + '</span>\
     <a class="calendarTime">' + time + '</a>\
  </li>');
 
 }
 
+
+
 function loadChairAnnouncements() {
-
-//https://sheets.googleapis.com/v4/spreadsheets/1ChIxEqBwPGLrEhcwdVmv6pv4fVwtu_xJ2gkw1aPnm-M/values/A2:C22?key=AIzaSyDyq-41653zwrojWvFKnuzM6L9Kh0DJk7s
-
-
-  $.ajax({
-    url: 'https://sheets.googleapis.com/v4/spreadsheets/1ChIxEqBwPGLrEhcwdVmv6pv4fVwtu_xJ2gkw1aPnm-M/values/A2:C22?key=AIzaSyDyq-41653zwrojWvFKnuzM6L9Kh0DJk7s',
-    dataType: 'jsonp',
-    success: function(data) {
-      //console.log(data["values"]);
-      for (var i = 0; i < data["values"].length; i++) {
-      //   console.log(data["values"][i]);
-        if(data["values"][i].length > 2) {
-          console.log(data["values"][i][0]);
+	$.ajax({
+		url: 'https://sheets.googleapis.com/v4/spreadsheets/1ChIxEqBwPGLrEhcwdVmv6pv4fVwtu_xJ2gkw1aPnm-M/values/A2:C22?key=AIzaSyDyq-41653zwrojWvFKnuzM6L9Kh0DJk7s',
+		dataType: 'jsonp',
+		success: function (data) {
+      do {
+        loadChairAnnouncements.curMessage++;
+        if(loadChairAnnouncements.curMessage >= data["values"].length) {
+          loadChairAnnouncements.curMessage = 0;
         }
-      }
+      } while (data["values"][loadChairAnnouncements.curMessage].length < 3);
 
-    }
-  });
+      var i = loadChairAnnouncements.curMessage;
 
+      $("#announcement").fadeOut(500, function() {
+        $("#announcementName").html(data["values"][i][1]);
+        $("#announcementPosition").html(data["values"][i][0]);
+        $("#announcementMessage").html(data["values"][i][2]);
+      });
+
+      $("#announcement").fadeIn(500);
+		}
+	});
 }
 
 
-$(document).ready(function()
-{
-  updateCalendars();
-  setInterval('updateCalendars()', 1000*60*60);
+$(document).ready(function () {
+	updateCalendars();
+	setInterval('updateCalendars()', 1000 * 60 * 60);
 
-  //loadChairAnnouncements();
+  loadChairAnnouncements.curMessage = 0;
+	loadChairAnnouncements();
+  setInterval('loadChairAnnouncements()', 10000);
 
-  setInterval('updateClock()', 2000);
+
+
+	setInterval('updateClock()', 2000);
 });
